dea08ed692b37ed32aa9b673f3a9eae87071840b
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-10-26 21:06:09 +0100

    helper function

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -6,7 +6,7 @@ exports.deleteOne = (Model) => async (req, res, next) => {
     try {
         const doc = await Model.findByIdAndDelete(req.params.id);
         if (!doc) return errorHandler(404, `The document with the given Id not found`);
-        return responseHandler(res, null, next, 200,  `Document deleted successfully`);
+        return responseHandler(res, null, next, 200, `Document deleted successfully`);
     } catch (error) {
         return res.status(500).json({ message: 'Fail', error: error.message });
     };
@@ -52,3 +52,12 @@ exports.findOne = (Model) => async (req, res, next) => {
     };
 };
 
+exports.createOne = (Model) => async (req, res, next) => {
+    try {
+        const doc = await Model.create(req.body);
+        const message = "Created successful";
+        return responseHandler(res, doc, next, 200, message, 1);
+    } catch (error) {
+        return res.status(500).json({ message: 'Fail', error: error.message });
+    }
+}
