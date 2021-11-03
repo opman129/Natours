06adeeb514b7c9fc1cac4cd11756e9a79b536a65
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-11-03 12:15:08 +0100

    booking controller

---
+++ src/controllers/bookingController.js
@@ -0,0 +1,10 @@
+const Tour = require('../models/tour');
+const { catchAsync } = require("../utils/catchAsync")
+
+exports.getCheckoutSession = catchAsync(async(req, res, next) => {
+    /** Get Currently Booked Tour */
+
+    /** Create session as response */
+
+    /** Send to client */
+});
--- src/routes/bookings/bookings.js
+++ src/routes/bookings/bookings.js
@@ -1,4 +1,7 @@
 const router = require('express').Router({ mergeParams: true });
 const Booking = require('../../controllers/bookingController');
+const { protect } = require('../../middleware/protect');
+
+router.get('/checkout-session/:tour_id', protect, Booking.getCheckoutSession);
 
 module.exports = router;
