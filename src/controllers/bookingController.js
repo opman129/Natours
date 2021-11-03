a868055d796a2158f04dc0147582dcbd6ce38f4a
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-11-03 12:34:53 +0100

    stripe checkout

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -1,10 +1,31 @@
 const Tour = require('../models/tour');
-const { catchAsync } = require("../utils/catchAsync")
+const stripe = require('stripe')(process.env.STRIPE_SK);
+const { catchAsync } = require("../utils/catchAsync");
+const responseHandler = require('../utils/responseHandler');
 
 exports.getCheckoutSession = catchAsync(async(req, res, next) => {
     /** Get Currently Booked Tour */
+    const tour = await Tour.findById(req.params.tour_id);
 
     /** Create session as response */
+    const session = await stripe.checkout.session.create({
+        payment_method_types: ['card'],
+        success_url: `${req.protocol}://${req.get('host')}/`,
+        cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+        customer_email: req.user.email,
+        client_reference_id: req.params.tour_id,
+        line_items: [
+            {
+                name: `${tour.title} Tour`,
+                description: tour.summary,
+                amount: tour.price * 100,
+                currency: 'usd',
+                quantity: 1
+            }
+        ]
+    });
 
     /** Send to client */
+    const message = 'Stripe session generated successfully';
+    return responseHandler(res, session, next, 201, message, 1);
 });
